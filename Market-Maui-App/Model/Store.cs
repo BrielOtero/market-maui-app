122f34d17d236261c547744be7dc5ce03725eed3
Gabriel O <info.otero.gabriel@gmail.com>
2023-01-17 21:54:26 +0100

    save

---
+++ Market-Maui-App/Model/Store.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market_Maui_App.Model;
+
+class Store
+{
+
+    public double Quantity_Store { get => Quantity_Store; set => Quantity_Store = value; }
+    public string Name { get => Name; set => Name = value; }
+
+    public Store(string name, double quantity_Store)
+    {
+        this.Name = name;
+        this.Quantity_Store = quantity_Store;
+    }
+
+    public Store()
+       : this("", 0)
+    {
+    }
+}
--- Market-Maui-App/ViewModel/ModifyPageViewModel.cs
+++ Market-Maui-App/ViewModel/ModifyPageViewModel.cs
@@ -9,7 +9,6 @@ public partial class ModifyPageViewModel : BaseViewModel
     {
         this.productService = productService;
     }
-
     [ObservableProperty]
     Product product;
 
@@ -29,67 +28,99 @@ public partial class ModifyPageViewModel : BaseViewModel
     private string measurement;
     [ObservableProperty]
     private string ref_Alcampo;
-    [ObservableProperty]
-    private double quantity_Alcampo;
+
     [ObservableProperty]
     private string ref_Carrefour;
     [ObservableProperty]
     private double quantity_Carrefour;
+    private double quantity_Alcampo;
 
+    ObservableCollection<Store> views = new();
 
-
-    [RelayCommand]
-    async Task DeStock()
+    public double Quantity_Alcampo
     {
-        if (Stock > 0)
-            Stock--;
-    }
-
-    [RelayCommand]
-    async Task InStock()
-    {
-        Stock++;
-    }
-
-    [RelayCommand]
-    async Task DeTarget()
-    {
-        if (Target > 0)
-            Target--;
-    }
-
-    [RelayCommand]
-    async Task InTarget()
-    {
-        Target++;
-    }
-
-    [RelayCommand]
-    internal async Task LoadValues()
-    {
-        this.Name = Product.Name;
-        this.Image = Product.Image;
-        this.Stock = Product.Stock;
-        this.Target = Product.Target_Stock;
-        this.Quantity = Product.Quantity;
-        this.Measurement = Product.Measurement;
-        this.Ref_Alcampo = Product.Ref_Alcampo;
-        this.Quantity_Alcampo = Product.Quantity_Alcampo;
-        this.Ref_Carrefour = Product.Ref_Carrefour;
-        this.Quantity_Carrefour = Product.Quantity_Carrefour;
-    }
-
-    [RelayCommand]
-    async Task Apply()
-    {
-        if (String.IsNullOrEmpty(Name) || String.IsNullOrEmpty(Image) ||  String.IsNullOrEmpty(Ref_Alcampo)|| Double.IsNaN(Quantity_Alcampo) || String.IsNullOrEmpty(Ref_Carrefour)|| Double.IsNaN(Quantity_Carrefour))
+        get => quantity_Alcampo;
+        set
         {
-            await Shell.Current.DisplayAlert("Error!", "Fill the fields ", "Ok");
-            return;
+            if (!global::System.Collections.Generic.EqualityComparer<double>.Default.Equals(quantity_Alcampo, value))
+            {
+                OnPropertyChanging(nameof(Quantity_Alcampo));
+                string name = "Alcampo";
+                Store store = views.First(x => x.Name == name);
+                quantity_Alcampo = value;
+
+                if (store == null)
+                {
+                    views.Add(new Store("Alcampo", quantity_Alcampo));
+                }
+                else
+                {
+                    views[views.IndexOf(store)] = new Store(name,quantity_Alcampo);
+                }
+
+
+
+            }
+
+            OnPropertyChanged(nameof(Quantity_Alcampo));
         }
-        var product = new Product(Name, Image, Stock, Target,Quantity,Measurement, Ref_Alcampo,Quantity_Alcampo, Ref_Carrefour,Quantity_Carrefour);
-        await productService.UpdateProduct(product,Product.Id);
-        await Shell.Current.GoToAsync("..", true);
     }
+}
+
+
+
+[RelayCommand]
+async Task DeStock()
+{
+    if (Stock > 0)
+        Stock--;
+}
+
+[RelayCommand]
+async Task InStock()
+{
+    Stock++;
+}
+
+[RelayCommand]
+async Task DeTarget()
+{
+    if (Target > 0)
+        Target--;
+}
+
+[RelayCommand]
+async Task InTarget()
+{
+    Target++;
+}
+
+[RelayCommand]
+internal async Task LoadValues()
+{
+    this.Name = Product.Name;
+    this.Image = Product.Image;
+    this.Stock = Product.Stock;
+    this.Target = Product.Target_Stock;
+    this.Quantity = Product.Quantity;
+    this.Measurement = Product.Measurement;
+    this.Ref_Alcampo = Product.Ref_Alcampo;
+    this.Quantity_Alcampo = Product.Quantity_Alcampo;
+    this.Ref_Carrefour = Product.Ref_Carrefour;
+    this.Quantity_Carrefour = Product.Quantity_Carrefour;
+}
+
+[RelayCommand]
+async Task Apply()
+{
+    if (String.IsNullOrEmpty(Name) || String.IsNullOrEmpty(Image) || String.IsNullOrEmpty(Ref_Alcampo) || Double.IsNaN(Quantity_Alcampo) || String.IsNullOrEmpty(Ref_Carrefour) || Double.IsNaN(Quantity_Carrefour))
+    {
+        await Shell.Current.DisplayAlert("Error!", "Fill the fields ", "Ok");
+        return;
+    }
+    var product = new Product(Name, Image, Stock, Target, Quantity, Measurement, Ref_Alcampo, Quantity_Alcampo, Ref_Carrefour, Quantity_Carrefour);
+    await productService.UpdateProduct(product, Product.Id);
+    await Shell.Current.GoToAsync("..", true);
+}
 
 }
